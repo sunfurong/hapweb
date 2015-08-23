9606169336c16c56782c035a50e897920214db76
sunfurong <sunfurong19921124@qq.com>
2015-08-23 18:56:48 +0800

    安装jade模板，安装html视频模板vedio.js，上传6个视频

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,13 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('home',{});
 });
-router.get('/images',function(reg,res,next){
-  res.render('secondViews/images',{});
-});
-router.get('/medias',function(reg,res,next){
-  res.render('secondViews/medias',{});
-});
-router.get('/words',function(reg,res,next){
-  res.render('secondViews/words',{});
-});
+//router.get('/images',function(reg,res,next){
+//  res.render('secondViews/images',{});
+//});
+//router.get('/medias',function(reg,res,next){
+//  res.render('secondViews/medias',{});
+//});
+//router.get('/words',function(reg,res,next){
+//  res.render('secondViews/words',{});
+//});
 module.exports = router;
